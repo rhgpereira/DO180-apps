5b9233dfc1b17dffbcc3a8f6c20d6164eb254e15
Gustavo Pereira <63612193+rhgpereira@users.noreply.github.com>
2020-05-10 00:03:23 -0300

    Updating the file again and again

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,6 +1,6 @@
 <?php
-print "Hello, World! php version is " . PHP_VERSION . "\n""\n""\n";
-print "This is a new message\n";
+print "Hello, World! php version is " . PHP_VERSION . "\n\n";
+print "This is a new message\n\n";
 print "This is a new message 2\n";
 
 ?>
